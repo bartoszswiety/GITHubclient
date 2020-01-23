612697859ab873ee943b2fb74983bda14f8765b0
Bartosz Swiety <>
2020-01-23 14:40:41 +0100

    API: Data Models

---
+++ XCodeProject/GitClient/Model/DataModel/GitProject.swift
@@ -0,0 +1,73 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let projectList = try ProjectList(json)
+
+import Foundation
+
+// MARK: - ProjectList
+class ProjectList: Codable {
+    let totalCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case totalCount
+    }
+
+    init(totalCount: Int) {
+        self.totalCount = totalCount
+    }
+}
+
+// MARK: ProjectList convenience initializers and mutators
+
+extension ProjectList {
+    convenience init(data: Data) throws {
+        let me = try newJSONDecoder().decode(ProjectList.self, from: data)
+        self.init(totalCount: me.totalCount)
+    }
+
+    convenience init(_ json: String, using encoding: String.Encoding = .utf8) throws {
+        guard let data = json.data(using: encoding) else {
+            throw NSError(domain: "JSONDecoding", code: 0, userInfo: nil)
+        }
+        try self.init(data: data)
+    }
+
+    convenience init(fromURL url: URL) throws {
+        try self.init(data: try Data(contentsOf: url))
+    }
+
+    func with(
+        totalCount: Int? = nil
+    ) -> ProjectList {
+        return ProjectList(
+            totalCount: totalCount ?? self.totalCount
+        )
+    }
+
+    func jsonData() throws -> Data {
+        return try newJSONEncoder().encode(self)
+    }
+
+    func jsonString(encoding: String.Encoding = .utf8) throws -> String? {
+        return String(data: try self.jsonData(), encoding: encoding)
+    }
+}
+
+// MARK: - Helper functions for creating encoders and decoders
+
+func newJSONDecoder() -> JSONDecoder {
+    let decoder = JSONDecoder()
+    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+        decoder.dateDecodingStrategy = .iso8601
+    }
+    return decoder
+}
+
+func newJSONEncoder() -> JSONEncoder {
+    let encoder = JSONEncoder()
+    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+        encoder.dateEncodingStrategy = .iso8601
+    }
+    return encoder
+}
