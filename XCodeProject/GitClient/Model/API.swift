ef1176cc6e6810ca6614465128be38e0e6cdb80d
Bartosz Swiety <>
2020-01-23 13:12:41 +0100

    API: class added

---
+++ XCodeProject/GitClient/Model/API.swift
@@ -0,0 +1,19 @@
+//
+//  API.swift
+//  GitClient
+//
+//  Created by Bartosz Swiety on 23/01/2020.
+//  Copyright © 2020 Bartosz Swiety. All rights reserved.
+//
+
+import Foundation
+
+public class API {
+// MARK: - Preporties
+    public static var shared: API = API()
+}
+
+extension API
+{
+    //MARK: -Request
+}
