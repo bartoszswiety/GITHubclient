3ad7468a70ef7534d959c67e84a10eed5296820e
Bartosz Swiety <>
2020-01-23 13:07:37 +0100

    Xcode project

---
+++ XCodeProject/GitClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitClient
+//
+//  Created by Bartosz Swiety on 23/01/2020.
+//  Copyright © 2020 Bartosz Swiety. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XCodeProject/GitClientTests/GitClientTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitClientTests.swift
+//  GitClientTests
+//
+//  Created by Bartosz Swiety on 23/01/2020.
+//  Copyright © 2020 Bartosz Swiety. All rights reserved.
+//
+
+import XCTest
+@testable import GitClient
+
+class GitClientTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
